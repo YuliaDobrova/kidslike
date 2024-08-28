eb25016531e38398c953ae564751de221bbed3e0
yuliiadobrova <dobrova2009@gmail.com>
2024-08-27 22:25:44 -0700

    planning tasks text fix

--- src/components/planningCards/planningCards/PlanningCards.jsx
+++ src/components/planningCards/planningCards/PlanningCards.jsx
@@ -1,25 +1,27 @@
-import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-import styles from './PlanningCards.module.css';
-import defaultImg from '../image/calendar.webp';
-import { patchActiveTask } from '../../../redux/tasks/tasksOperation';
-import PlanningCardItem from '../planningCardItem/PlanningCardItem';
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { useTranslation } from "react-i18next";
+import { patchActiveTask } from "../../../redux/tasks/tasksOperation";
+import PlanningCardItem from "../planningCardItem/PlanningCardItem";
+import defaultImg from "../image/calendar.webp";
+import styles from "./PlanningCards.module.css";
 
 const PlanningCards = ({ tasks }) => {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
 
   const onAddActiveTask = useCallback(
     (taskId, dataObj) => {
       dispatch(patchActiveTask(taskId, dataObj));
     },
-    [dispatch],
+    [dispatch]
   );
 
   return (
     <div className={styles.cartContainer}>
       {tasks.length > 0 ? (
         <ul className={styles.cardList}>
-          {tasks.map(task => (
+          {tasks.map((task) => (
             <PlanningCardItem
               onAddActiveTask={onAddActiveTask}
               task={task}
@@ -28,9 +30,11 @@ const PlanningCards = ({ tasks }) => {
           ))}
         </ul>
       ) : (
-        <h2 className={styles.createListHabits}>
-          Создайте свой список привычек
-        </h2>
+        <>
+          <h2 className={styles.createListHabits}>
+            {t("Create your own habit list")}
+          </h2>
+        </>
       )}
     </div>
   );
